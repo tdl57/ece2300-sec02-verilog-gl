d1629a2b345fb89d24eafd7769eea878faf34fa8
tdl57 <tdl57@cornell.edu>
2024-09-22 16:25:47 -0400

    ?

--- PairTripleDetector_GL.v
+++ PairTripleDetector_GL.v
@@ -16,7 +16,13 @@ module PairTripleDetector_GL
   //''' ACTIVITY '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   // Implement pair/triple detector using explicit gate-level modeling
   //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-
+  wire w;
+  wire y;
+  wire x;
+  or(w, in0, in1);
+  and(x, in0, in1);
+  and(y, w, in2);
+  or(out, y, x)
 endmodule
 
 `endif /* PAIR_TRIPLE_DETECTOR_GL_V */
